371e72834d0fcba4800d2c9d7ce4680580a6e8cb
Alex Martinez <alexmartinezmoreno@fpllefia.com>
2020-09-26 21:56:58 +0000

    add comments practice

---
+++ N1P1ALEXMARTINEZComment.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <h1 class="text-center">Comments for PAC01</h1>
+        
+        <h3>List of learn concepts</h3>
+        <ol>
+            <li>How to install Apache</li>
+            <li>How to configure Apache</li>
+            <li>How to install php and configure with Apache</li>
+            <li>How to install MySQL Server</li>
+            <li>How to stop and start the services with powershell</li>
+            <li>How to use CODENVY and create a new workspaces</li>
+            <li>How to implement php in our CODENVY workspaces</li>
+            <li>How to clone projects from GIT</li>
+            <li>How to run MySQL server</li>
+            <li>How to check databases connections in php</li>
+        </ol>
+        <h3>Document and teacher explanation</h3>
+        <p>I'm surprised about all the new content that you've created, it's amazing. I like the YouTube tutorials.
+            I think is a good idea define steps and all the configurations for the programs that we need. It's a lot of save time. 
+        </p>
+        <h3>Mark</h3>
+        <p>
+            I don't like mark myself. I always try to do my best.
+        </p>
+        <h3>Improvements</h3>
+        <p>I don't see any improvement. It seems fine.</p>
+    </div>
+
+</body>
+</html>
